9a90c6ccc1cb46a906473b2702f442e8cdc51aea
Alan Mortoni <mortoni.alan@hotmail.com>
2023-11-08 08:44:12 +1000

    fix(component): format

--- src/QuadraticVote/Diamond.tsx
+++ src/QuadraticVote/Diamond.tsx
@@ -1,41 +1,41 @@
-import { cloneElement, useMemo } from "react";
-import { useQuadraticVote } from ".";
-import { createDiamond, setViewBox } from "./utils";
+import React, { cloneElement, useMemo } from 'react'
+import { useQuadraticVote } from '.'
+import { createDiamond, setViewBox } from './utils'
 
-interface DiamondProps {
-  id: number;
-  neutralColor?: string;
-  positiveColor?: string;
-  negativeColor?: string;
-  circleRadius?: number;
+export interface DiamondProps {
+  id: number
+  neutralColor?: string
+  positiveColor?: string
+  negativeColor?: string
+  circleRadius?: number
 }
 
 const Diamond: React.FC<DiamondProps> = ({
   id,
-  neutralColor = "#A9A9A9",
-  positiveColor = "#00FF00",
-  negativeColor = "#FF0000",
+  neutralColor = '#A9A9A9',
+  positiveColor = '#00FF00',
+  negativeColor = '#FF0000',
   circleRadius = 4,
 }) => {
-  const { questions, credits } = useQuadraticVote();
+  const { questions, credits } = useQuadraticVote()
 
   const circles = useMemo(
     () => createDiamond(id, credits, circleRadius),
-    [id, credits, circleRadius]
-  );
+    [id, credits, circleRadius],
+  )
 
   const question = useMemo(
     () => questions.find((q) => q.id === id),
-    [questions, id]
-  );
-  const voteLevel = question ? Math.abs(question.vote) : 0;
+    [questions, id],
+  )
+  const voteLevel = question ? Math.abs(question.vote) : 0
   const voteColor = question
     ? question.vote > 0
       ? positiveColor
       : negativeColor
-    : neutralColor;
+    : neutralColor
 
-  const viewBox = useMemo(() => setViewBox(circles), [circles]);
+  const viewBox = useMemo(() => setViewBox(circles), [circles])
 
   return (
     <svg
@@ -45,18 +45,16 @@ const Diamond: React.FC<DiamondProps> = ({
     >
       <g>
         {circles.map((circle) => {
-          const circleLevel = parseInt(
-            circle.props["data-level"].split("-")[1]
-          );
-          const fillColor = circleLevel <= voteLevel ? voteColor : neutralColor;
+          const circleLevel = parseInt(circle.props['data-level'].split('-')[1])
+          const fillColor = circleLevel <= voteLevel ? voteColor : neutralColor
 
           return cloneElement(circle, {
-            style: { fill: fillColor, transition: "fill 0.35s ease-out" },
-          });
+            style: { fill: fillColor, transition: 'fill 0.35s ease-out' },
+          })
         })}
       </g>
     </svg>
-  );
-};
+  )
+}
 
-export default Diamond;
+export default Diamond
