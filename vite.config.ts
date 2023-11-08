564fabfc64f7e24f8f72cb8b35fd493a4ef3e806
Alan Mortoni <mortoni.alan@hotmail.com>
2023-11-08 21:08:03 +1000

    fix(vite): entry point

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,29 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import react from '@vitejs/plugin-react'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import * as packageJson from './package.json'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/QuadraticVote/index.ts'),
+      entry: resolve('src', 'QuadraticVote/index.ts'),
       name: 'quadratic-vote',
-      fileName: 'quadratic-vote',
+      formats: ['es', 'umd'],
+      fileName: (format) => `quadratic-vote.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.dependencies)],
     },
   },
-  plugins: [dts()],
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    dts({
+      include: ['src/QuadraticVote'],
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
